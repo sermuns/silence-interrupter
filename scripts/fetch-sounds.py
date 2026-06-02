e483fa6683351947a55657968c3ca53ae928ec59
Samuel Åkesson <sermuns@lysator.liu.se>
2026-06-02 20:41:40 +0200

    add villager sounds

--- scripts/fetch-sounds.py
+++ scripts/fetch-sounds.py
@@ -13,17 +13,26 @@ OUT_DIR = Path("audio")
 
 
 async def download(session, url):
-    filename = Path(url).name
+    file_name = Path(url).name
+    file_path = OUT_DIR / file_name
+
+    if file_path.exists():
+        print(f"'{file_name}' already exists, skipping")
+        return
+
+    print(f"Downloading '{file_name}'")
 
     async with session.get(url) as resp:
         resp.raise_for_status()
-        (OUT_DIR / filename).write_bytes(await resp.read())
+        (OUT_DIR / file_name).write_bytes(await resp.read())
 
 
 async def main():
     OUT_DIR.mkdir(exist_ok=True)
 
-    urls = [f"https://minecraft.wiki/images/Cave{i}.ogg" for i in range(1, 20)]
+    urls = [f"https://minecraft.wiki/images/Cave{i}.ogg" for i in range(1, 20)] + [
+        f"https://minecraft.wiki/images/Villager_idle{i}.ogg" for i in range(1, 4)
+    ]
 
     async with aiohttp.ClientSession() as session:
         await asyncio.gather(*(download(session, url) for url in urls))
